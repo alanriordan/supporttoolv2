85166832ba012191b75cf0e8af327f99e34dc630
alanriordan <riordanalan@yahoo.com>
2020-10-22 07:26:12 +0100

    Reorganized into different modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import {Component, VERSION, ViewEncapsulation} from '@angular/core';
-
-import {NavItem} from './navigation/nav-item';
+import {Component, OnInit, VERSION, ViewEncapsulation} from '@angular/core';
 import {navItems} from './navigation/nav-data';
+import {EnvService} from './service/env.service';
+import {Env} from './common/env.enum';
+import {JmsService} from './jms/service/jms.service';
 
 @Component({
     selector: 'app-root',
@@ -9,8 +10,27 @@ import {navItems} from './navigation/nav-data';
     styleUrls: ['app.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     version = VERSION;
     navItems = navItems;
-    env;
+    env: string;
+    envs = [];
+
+    constructor(private envService: EnvService, private jmsService: JmsService) {
+
+    }
+
+    setEnv(env: Env): void {
+        this.envService.setEnv(env);
+    }
+
+    ngOnInit(): void {
+        for (const env in Env) {
+            if (isNaN(Number(env))) {
+                this.envs.push(env);
+            }
+        }
+        this.envService.setEnv(Env.SIT);
+        this.env = Env[Env.SIT];
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,139 +3,52 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {A11yModule} from '@angular/cdk/a11y';
-import {BidiModule} from '@angular/cdk/bidi';
-import {ObserversModule} from '@angular/cdk/observers';
-import {OverlayModule} from '@angular/cdk/overlay';
-import {PlatformModule} from '@angular/cdk/platform';
-import {PortalModule} from '@angular/cdk/portal';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
 import {AppComponent} from './app.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MenuItemComponent} from './menu-item/menu-item.component';
 import {AppRoutingModule} from './app-routing.module';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatInputModule} from '@angular/material/input';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatCardModule} from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatRadioModule} from '@angular/material/radio';
-
-import {MatChipsModule} from '@angular/material/chips';
-import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import { JmsListenerComponent } from './jms-listener/jms-listener.component';
-import { EmailConfigComponent } from './email-config/email-config.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { LdapInfoComponent } from './ldap-info/ldap-info.component';
-import { JmsSenderComponent } from './jms-sender/jms-sender.component';
-import { EmailSenderComponent } from './email-sender/email-sender.component';
+import {EmailConfigComponent} from './email-config/email-config.component';
+import {UserDetailsComponent} from './user-details/user-details.component';
+import {EmailSenderComponent} from './email-sender/email-sender.component';
 import {HttpClientModule} from '@angular/common/http';
-import {JmsService} from './jms.service';
-import { JmsMessageTemplateComponent } from './jms-message-template/jms-message-template.component';
-import { TestFormComponent } from './test-form/test-form.component';
+import {EnvService} from './service/env.service';
+import {VendorModule} from './vendor.module';
+import {LdapModule} from "./ldap/ldap.module";
+import {JmsModule} from "./jms/jms.module";
+import {MatCard} from "@angular/material/card";
 
-/**
- * NgModule that includes all Material modules that are required.
- */
-@NgModule({
-    exports: [
-        // CDK
-        A11yModule,
-        BidiModule,
-        ObserversModule,
-        OverlayModule,
-        PlatformModule,
-        PortalModule,
-
-        CdkStepperModule,
-        CdkTableModule,
-
-        // Material
-        MatAutocompleteModule,
-        MatButtonModule,
-        MatButtonToggleModule,
-        MatCardModule,
-        MatCheckboxModule,
-        MatChipsModule,
-        MatDatepickerModule,
-        MatDialogModule,
-        MatExpansionModule,
-        MatGridListModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        MatMenuModule,
-        MatProgressBarModule,
-        MatProgressSpinnerModule,
-        MatRadioModule,
-        MatRippleModule,
-        MatSelectModule,
-        MatSidenavModule,
-        MatSlideToggleModule,
-        MatSliderModule,
-        MatSnackBarModule,
-        MatTabsModule,
-        MatToolbarModule,
-        MatTooltipModule,
-        MatNativeDateModule,
-    ],
-    imports: [
-
-    MatInputModule,
-
-    MatButtonModule,
-
-    MatSelectModule,
-
-    MatRadioModule,
-
-    MatCardModule,
-
-    ReactiveFormsModule],
-    declarations: [TestFormComponent]
-})
-export class MaterialModule {
-}
 
 @NgModule({
     imports: [
         BrowserModule,
         CommonModule,
-        MaterialModule,
+        VendorModule,
         HttpClientModule,
         FormsModule,
         ReactiveFormsModule,
         BrowserAnimationsModule,
         FlexLayoutModule,
-        AppRoutingModule
+        AppRoutingModule,
+        LdapModule,
+        JmsModule
     ],
     declarations: [
         AppComponent,
-        MenuItemComponent, JmsListenerComponent, EmailConfigComponent, UserDetailsComponent, LdapInfoComponent,
-        JmsSenderComponent, EmailSenderComponent, JmsMessageTemplateComponent
+        MenuItemComponent, EmailConfigComponent, UserDetailsComponent,
+        EmailSenderComponent
     ],
     bootstrap: [AppComponent],
-    providers: [JmsService]
+    providers: [EnvService],
+    exports: [BrowserModule,
+        CommonModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        AppRoutingModule,
+        VendorModule,
+    MatCard]
 })
 export class AppModule {
 }
